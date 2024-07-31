19e2f56cb6d2dcd4622e1c0d873025398f340a73
seonmin5 <seonmin5555@gmail.com>
2024-07-31 10:04:28 +0900

    [FE] dummy data

--- budgetChallenge/src/constants/dummyData.js
+++ budgetChallenge/src/constants/dummyData.js
@@ -1,7 +1,13 @@
 export const DummyData_History = {
-    id: 1,
-    date: '2024/07/31',
-    mini_Text: '식사',
-    itemName: '샌드위치',
-    itemCost: '5000'
-}
+  id: 1,
+  date: "2024/07/31",
+  mini_Text: "식사",
+  itemName: "샌드위치",
+  itemCost: "5000",
+};
+
+export const DummyData_Budget = {
+  id: 1,
+  budgetTitle: "5만원 챌린지",
+  budgetAmount: 50000,
+};
