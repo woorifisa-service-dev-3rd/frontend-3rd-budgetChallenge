f05ff68d651c693bd6e96dd546602b4fb9a37169
Jieun-KWAK <jieun47631@gmail.com>
2024-07-31 10:12:38 +0900

    dummyData

--- budgetChallenge/src/constants/dummyData.js
+++ budgetChallenge/src/constants/dummyData.js
@@ -1,7 +1,16 @@
-export const DummyData_History = {
-    id: 1,
-    date: '2024/07/31',
-    mini_Text: '식사',
-    itemName: '샌드위치',
-    itemCost: '5000'
-}
+export const DummyData_History = [
+    {
+        id: 1,
+        date: '2024/07/31',
+        mini_Text: '식사',
+        itemName: '샌드위치',
+        itemCost: '5000'
+    }, 
+    {
+        id: 2,
+        date: '2024/08/01',
+        mini_Text: '쇼핑',
+        itemName: '반팔티',
+        itemCost: '50000'
+    }, 
+]
