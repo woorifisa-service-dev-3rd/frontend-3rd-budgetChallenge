523918f7ca1e2b4c9b77353513dbc33cc9955f05
Jieun-KWAK <jieun47631@gmail.com>
2024-08-01 09:59:35 +0900

    create&read 기능 구현

---
+++ budgetChallenge/src/components/history/History.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import HistoryBody from './HistoryBody'
+import HistoryHeader from './HistoryHeader'
+import { DummyData_History } from '../../constants/dummyData'
+
+const History = () => {
+  const [historyforms, setHistoryforms] = useState(DummyData_History);
+
+  const addHistoryFormoHandler = ({ date, miniText, itemName, itemCost }) => {
+
+    const newHistory = {
+      id: self.crypto.randomUUID(),
+      date,
+      miniText,
+      itemName,
+      itemCost
+    }
+
+    const updatedHistory = [...historyforms, newHistory];
+    setHistoryforms(updatedHistory);
+  }
+
+  return (
+    <div>
+      <h2>사용내역</h2>
+      <HistoryHeader onAdd={addHistoryFormoHandler}/>
+      <HistoryBody historyforms={historyforms}/>
+    </div>
+  )
+}
+
+export default History
---
+++ budgetChallenge/src/constants/dummyData.js
@@ -0,0 +1,16 @@
+export const DummyData_History = [
+    {
+        id: 1,
+        date: '2024/07/31',
+        miniText: '식사',
+        itemName: '샌드위치',
+        itemCost: '5000'
+    }, 
+    {
+        id: 2,
+        date: '2024/08/01',
+        miniText: '쇼핑',
+        itemName: '반팔티',
+        itemCost: '50000'
+    }, 
+]
